cc7c42170660b2e7340a175a1b443212bf40dc6a
Ina <dlsdk0601@gmail.com>
2021-08-25 02:22:05 +0900

    v-3

--- src/canvas.js
+++ src/canvas.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const con1 = document.querySelector(".main_video");
 const video = document.querySelector("#main .main_video video");
 const timer = document.querySelectorAll("#timer");
 const banner = document.querySelectorAll("#main .main_video .slides .banner");
 const banner_background = document.querySelectorAll("#main .main_video .slides .banner .back");
 const little_title = document.querySelectorAll("#little_title");
 const sub_text = document.querySelectorAll("#sub_text");
+const con1_title = document.querySelectorAll(".main_video .slides .title h2");
 const video_Width = 1920;
 const video_Height = 1080;
 const windowHeight = "100vh";
@@ -19,12 +21,16 @@ let xpos2 = 0;
 video.currentTime = 19;
 
 //Function-----------------------------------------------
+function intro(){
+    
+}
 function bannerActiveRemove(num){
     if(num != null) {
         banner_background[num].classList.remove("active");
         timer[num].classList.remove("active");
         little_title[num].classList.remove("active");
         sub_text[num].classList.remove("active");
+        con1_title[num].classList.remove("active");
     }
     return;
 }
@@ -33,6 +39,7 @@ function bannerActiveAdd(index){
     sub_text[index].classList.add("active");
     banner_background[index].classList.add("active");
     timer[index].classList.add("active");
+    con1_title[index].classList.add("active");
 }
 function bannerFunction(){
         bannerActiveRemove(num);
@@ -57,7 +64,7 @@ function draw(){
     // ctx.drawImage(image, 
     // sx(원본 x위치), sy(원본 y위치), sWidth(그려지길 원하는 위드값), sHeight(그려지길 원하는 하이트값), 
     // dx(캔버스 x위치), dy(캔버스 y위치), dWidth(그려지는 위드값), dHeight(그려지는 하이트값));
-    const canvas_left_prev = document.querySelector(".main_video .cvs");
+    const canvas_left_prev = document.querySelector(".main_video .next");
     const context = canvas_left_prev.getContext("2d");
 
     left_endpoint = ( windowWidth / 2 ) + 240 * ( num - 2 );
@@ -77,6 +84,19 @@ function draw(){
     requestAnimationFrame(draw);
 }
 
+function drawing(){
+    const newCanvas = document.createElement("canvas");
+    newCanvas.textContent = "이 브라우저는 캔버스를 지원하지 않습니다";
+    newCanvas.id = "cvs";
+    newCanvas.classList.add("cvs");
+    newCanvas.classList.add("next");
+    newCanvas.width = 1920;
+    newCanvas.height = 1080;
+    con1.prepend(newCanvas);
+
+
+}
+
 //banner 영상 재생시간 및 타이머 시간 7초!-------------------------
 let loop1 = setInterval(bannerFunction, 7000);
 bannerActiveAdd(0);
@@ -88,4 +108,7 @@ for(let i = 0; i < banner.length; i++){
         loop1 = setInterval(bannerFunction, 7000);
     });
 }
-draw();
+
+
+
+// DOMContentLoaded
--- src/con3_Event.js
+++ src/con3_Event.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const con3 = document.querySelector(".con3 .right_content .subs .more_box");
 const con3_prev_btn = document.querySelector(".con3 .swiper-button-prev");
 const con3_next_btn = document.querySelector(".con3 .swiper-button-next");
 const con3_cursor = document.querySelector("#con3_cursor");
@@ -6,7 +9,7 @@ const con3_arr_right = document.querySelector("#con3_cursor .con3_arr_right");
 const sub_title_el = document.querySelector(".con3 .right_content .subs h2");
 const sub_text_el = document.querySelector(".con3 .right_content .subs .subs_text");
 const sub_color_text_el = document.querySelector(".con3 .right_content .color_font");
-const timer_name = ["제품", "효능", "분석", "화장품", "글로벌"];
+const timer_name = ["제품분야", "효능분야", "분석분야", "화장품분야", "글로벌분야"];
 const sub_text_arr = ["최고를 향해 바른길을 걷기 위하여",
     "인류의 건강한 삶을 위하여", "끊임없는 도전과 혁신의 여정", 
     "홍삼의 힘을 피부에 전달하는 독자적 기술", "세계적 명품의 자리를 지키기 위하여"];
@@ -15,84 +18,24 @@ const sub_color_text = ["차별화된 제품개발 기술과 신소재 발굴로
 "인삼 재배기술, 품종개발 등의 분석연구로 R&D기술을 선도합니다.",
 "홍삼의 피부효능 연구와 차별화된 제품개발로 뷰티시장을 이끕니다.",
 "해외 수출제품 개발과 현지연구로 명품의 가치를 세계에 전파합니다."];
-const color_className = ["#B97924", "#438DB0", "#7C9E46", "#D15C3F", "#D15C3F"];
-let text_loop_num = 0
+let text_loop_num = 1;
+const sub =[];
 
+//sub put Tag
+sub.push(sub_title_el);
+sub.push(sub_text_el);
+sub.push(sub_color_text_el);
 
-//text animation
-// sub_title_el.classList.add("up");
-// sub_text_el.classList.add("up");
-// sub_color_text_el.classList.add("up");
-const sub_text_Change = () => {
-    sub_title_el.classList.remove("up");
-    sub_text_el.classList.remove("up");
-    sub_color_text_el.classList.remove("up");
-
-    sub_title_el.textContent = timer_name[text_loop_num] + "분야";
-    sub_text_el.textContent = sub_text_arr[text_loop_num];
-    sub_color_text_el.textContent = sub_color_text[text_loop_num];
-    sub_color_text_el.style.color = color_className[text_loop_num];
-
-    sub_title_el.classList.add("up");
-    sub_text_el.classList.add("up");
-    sub_color_text_el.classList.add("up");
-    text_loop_num++;
-    if(text_loop_num == 6) text_loop_num = 0;
+//text animation function
+const sub_text_change = () => {
+    for(let i = 0; i < sub.length; i++){
+        setTimeout(function(){
+            sub[i].style.opacity = 1;
+            sub[i].style.transform = "translateY(0)";
+        }, 500*i);
+    }
 }
-let text_loop = setInterval(sub_text_Change, 7000);
-
-
-//swiper Event-----------------------------------------------------------------------------
-let con3_swiper = new Swiper("#con3_left_content", {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-        delay: 7000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    controller: {
-        inverse: true,
-    },
-});
-let con3_swiper_right = new Swiper("#right_content", {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    centeredSlides: true,
-    direction: "vertical",
-    loop: true,
-    autoHeight: true,
-    autoplay: {
-        delay: 7000,
-        disableOnInteraction: false,
-    },
-    // pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true,
-    // },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-
-//swiper timbar Text put in
-const timer_spans = document.querySelectorAll(".con3 .swiper-pagination .swiper-pagination-bullet"); 
-//이거 여기 이 행 아닌곳에 쓰면 안잡힘.
-for(let i = 0; i < timer_spans.length; i++){
-    timer_spans[i].textContent = timer_name[i];
-}
-console.log(timer_spans);
-
+sub_text_change();
 
 //arrow image
 con3_prev_btn.addEventListener("mouseenter", () => {
@@ -127,4 +70,71 @@ con3_prev_btn.addEventListener("mousemove", (e) => {
 });
 
 
-// window.addEventListener('DOMContentLoaded', function(){});
+function con3_init(){
+    // let text_loop = setInterval(sub_text_Change, 7000);
+    //swiper Event-----------------------------------------------------------------------------
+    let con3_swiper = new Swiper("#con3_left_content", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        controller: {
+            inverse: true,
+        },
+    });
+
+    // con3_swiper.on("slideChange", function(){
+    //     sub_text_Change()
+    //     text_loop_num--;
+    //     if(text_loop_num == -1) text_loop_num = 4;
+    // })
+
+    let con3_swiper_right = new Swiper("#right_content", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        direction: "vertical",
+        loop: true,
+        autoHeight: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    // con3_swiper_right.on("slideChange", function(){
+    //     sub_text_Change()
+    //     text_loop_num++;
+    //     if(text_loop_num == 5) text_loop_num = 0;
+    // })
+
+
+    //swiper timbar Text put in--------------------------------------------------
+    const timer_spans = document.querySelectorAll(".con3 .swiper-pagination .swiper-pagination-bullet"); 
+    //이거 여기 이 행 아닌곳에 쓰면 안잡힘.
+    for(let i = 0; i < timer_spans.length; i++){
+        timer_spans[i].textContent = timer_name[i];
+    }
+}
+con3_init();
