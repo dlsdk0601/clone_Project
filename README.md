7731dcce98621c8c20d1711f903ac9d2c2839fb3
Ina <dlsdk0601@gmail.com>
2021-08-25 17:12:21 +0900

    readme write

--- README.md
+++ README.md
@@ -1,11 +1,81 @@
-"# clone_Project" 
+#Clone_Project
+___
+> Languages
+
+<img src="img/languages.png"  width="500" height="300">
+
+> 클론 사이트
+
+원작 사이트의 링크 [KGC인삼공사](https://www.kgc.co.kr/index.do)
+
 
 > 기획 의도 
-다양한 기능이 들어 있는 홈페이지를 선정하여 클론 코딩을 해봄으로써, 벤치 마킹에 대한 발판을 마련 할 수 있고
-다른 개발자들의 테크닉을 이해하는 연습을 할 수 있습니다.
 
-> 원작 사이트
-원작 사이트의 링크 [here](https://www.kgc.co.kr/index.do)
+* 실제 현업에서 어떠한 UI/UX를 구현하는지 세세한 관찰과 연구 목적
+
+* 난이도가 있는 기능 구현 연습​
+
+* 팀 프로젝트 진행 시, 팀원간에 코드 스킬 이해도 상승​
 
 > 주요 기능
+
+* **Canvas** 태그 제어
+```
+function draw(){
+    // ctx.drawImage(image,  sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    const canvas_prev = document.querySelector(".main_video .next");
+    const context = canvas_left_prev.getContext("2d");
+
+    left_endpoint = ( windowWidth / 2 ) + 240 * ( num - 2 );
+    right_startpoint = left_endpoint;
+    
+    context.clearRect(0, 0, canvas_left_prev.width, canvas_left_prev.height );
+
+    context.drawImage(video, 0, 0, xPos1, 1080,  0, 0, xPos1, canvas_left_prev.height);
+    context.drawImage(video, left_endpoint, 0, 1920-left_endpoint, video_Height, 
+                        right_startpoint, 0, xpos2, video_Height);
+
+    if(xPos1 < left_endpoint){
+        xPos1 += 40;
+    }
+    if(xpos2 < canvas_left_prev.width - left_endpoint){
+        xpos2 += 40;
+    }
+    requestAnimationFrame(draw);
+}
+```
+
+* **Swiper** Plugin 사용
+```
+let con3_swiper_right = new Swiper("#right_content", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        direction: "vertical",
+        loop: true,
+        autoHeight: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    //슬라이더 변경 될 시, 실행되는 함수
+    con3_swiper_right.on("slideChange", function(){
+        sub_text_hide();
+        text_loop_num = con3_swiper_right.realIndex;
+        sub_text_animation();
+    })
+```
+* **Slide** 제작
+
+> GithubPage
+
+클론페이지 [바로가기](https://dlsdk0601.github.io/clone_Project/)
+
 
--- src/con3_Event.js
+++ src/con3_Event.js
@@ -6,38 +6,123 @@ const con3_next_btn = document.querySelector(".con3 .swiper-button-next");
 const con3_cursor = document.querySelector("#con3_cursor");
 const con3_arr_left = document.querySelector("#con3_cursor .con3_arr_left");
 const con3_arr_right = document.querySelector("#con3_cursor .con3_arr_right");
-const sub_title_el = document.querySelector(".con3 .right_content .subs h2");
-const sub_text_el = document.querySelector(".con3 .right_content .subs .subs_text");
-const sub_color_text_el = document.querySelector(".con3 .right_content .color_font");
-const timer_name = ["제품분야", "효능분야", "분석분야", "화장품분야", "글로벌분야"];
-const sub_text_arr = ["최고를 향해 바른길을 걷기 위하여",
-    "인류의 건강한 삶을 위하여", "끊임없는 도전과 혁신의 여정", 
-    "홍삼의 힘을 피부에 전달하는 독자적 기술", "세계적 명품의 자리를 지키기 위하여"];
-const sub_color_text = ["차별화된 제품개발 기술과 신소재 발굴로 혁신을 이룹니다.",
-"홍삼 및 천연물 효능과 원료 안전성 연구로 건강한 미래를 만듭니다.",
-"인삼 재배기술, 품종개발 등의 분석연구로 R&D기술을 선도합니다.",
-"홍삼의 피부효능 연구와 차별화된 제품개발로 뷰티시장을 이끕니다.",
-"해외 수출제품 개발과 현지연구로 명품의 가치를 세계에 전파합니다."];
-let text_loop_num = 1;
-const sub =[];
+let text_loop_num = 0;
+const timer_name = ["제품", "효능", "분석", "화장품", "글로벌"];
+const txt = document.querySelectorAll(".con3 .right_content .subs .txt");
 
-//sub put Tag
-sub.push(sub_title_el);
-sub.push(sub_text_el);
-sub.push(sub_color_text_el);
 
-//text animation function
-const sub_text_change = () => {
-    for(let i = 0; i < sub.length; i++){
-        setTimeout(function(){
+// function
+const sub_text_hide = () => {
+    const txt_h2_prev = txt[text_loop_num].querySelector("h2");
+    const txt_subs_text_prev = txt[text_loop_num].querySelector(".subs_text");
+    const txt_color_font_prev = txt[text_loop_num].querySelector(".color_font");
+    const sub =[];
+
+    sub.push(txt_h2_prev);
+    sub.push(txt_subs_text_prev);
+    sub.push(txt_color_font_prev);
+
+    for(let i = 0; i < 3; i++){
+        setTimeout( () => {
+            sub[i].style.opacity = 0;
+            sub[i].style.transform = "translate(0, 0)";
+        }, 300*i);
+    }
+
+}
+const sub_text_animation = () => {
+
+    const txt_h2 = txt[text_loop_num].querySelector("h2");
+    const txt_subs_text = txt[text_loop_num].querySelector(".subs_text");
+    const txt_color_font = txt[text_loop_num].querySelector(".color_font");
+    const sub =[];
+
+    sub.push(txt_h2);
+    sub.push(txt_subs_text);
+    sub.push(txt_color_font);
+    
+    for(let i = 0; i < 3; i++){
+        setTimeout( () => {
             sub[i].style.opacity = 1;
-            sub[i].style.transform = "translateY(0)";
-        }, 500*i);
+            sub[i].style.transform = "translate(0, -20px)";
+        }, 300*(i+1));
+    }
+
+}
+function con3_init(){
+    //swiper Event-----------------------------------------------------------------------------
+    let con3_swiper = new Swiper("#con3_left_content", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        controller: {
+            inverse: true,
+        },
+    });
+
+    // con3_swiper.on("slideChange", function(){
+        
+    //     sub_text_hide();
+    //     text_loop_num--;
+    //     if(text_loop_num == -1){
+    //         text_loop_num = 4;
+    //     } 
+    //     sub_text_animation();
+    // })
+
+    let con3_swiper_right = new Swiper("#right_content", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+        direction: "vertical",
+        loop: true,
+        autoHeight: true,
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    con3_swiper_right.on("slideChange", function(){
+        sub_text_hide();
+        text_loop_num = con3_swiper_right.realIndex;
+        sub_text_animation();
+    })
+
+
+    //swiper timbar Text put in--------------------------------------------------
+    const timer_spans = document.querySelectorAll(".con3 .swiper-pagination .swiper-pagination-bullet"); 
+    //이거 여기 이 행 아닌곳에 쓰면 안잡힘.
+    for(let i = 0; i < timer_spans.length; i++){
+        timer_spans[i].textContent = timer_name[i];
     }
 }
-sub_text_change();
 
-//arrow image
+
+
+//arrow image Event
 con3_prev_btn.addEventListener("mouseenter", () => {
     con3_cursor.classList.add("on");
     con3_cursor.style.left = "0px";
@@ -70,71 +155,6 @@ con3_prev_btn.addEventListener("mousemove", (e) => {
 });
 
 
-function con3_init(){
-    // let text_loop = setInterval(sub_text_Change, 7000);
-    //swiper Event-----------------------------------------------------------------------------
-    let con3_swiper = new Swiper("#con3_left_content", {
-        speed: 500,
-        spaceBetween: 0,
-        slidesPerView: 1,
-        centeredSlides: true,
-        loop: true,
-        autoplay: {
-            delay: 7000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        controller: {
-            inverse: true,
-        },
-    });
-
-    // con3_swiper.on("slideChange", function(){
-    //     sub_text_Change()
-    //     text_loop_num--;
-    //     if(text_loop_num == -1) text_loop_num = 4;
-    // })
-
-    let con3_swiper_right = new Swiper("#right_content", {
-        speed: 500,
-        spaceBetween: 0,
-        slidesPerView: 1,
-        centeredSlides: true,
-        direction: "vertical",
-        loop: true,
-        autoHeight: true,
-        autoplay: {
-            delay: 7000,
-            disableOnInteraction: false,
-        },
-        // pagination: {
-        //     el: ".swiper-pagination",
-        //     clickable: true,
-        // },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-    // con3_swiper_right.on("slideChange", function(){
-    //     sub_text_Change()
-    //     text_loop_num++;
-    //     if(text_loop_num == 5) text_loop_num = 0;
-    // })
-
-
-    //swiper timbar Text put in--------------------------------------------------
-    const timer_spans = document.querySelectorAll(".con3 .swiper-pagination .swiper-pagination-bullet"); 
-    //이거 여기 이 행 아닌곳에 쓰면 안잡힘.
-    for(let i = 0; i < timer_spans.length; i++){
-        timer_spans[i].textContent = timer_name[i];
-    }
-}
+//active
+sub_text_animation();
 con3_init();
